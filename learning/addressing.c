e1c7bc260391693047e643e83a498214099b0bf3
ChronoAnomaly <chronoanomaly@gmail.com>
2015-07-07 10:12:29 -0400

    changed the code a little bit

--- learning/addressing.c
+++ learning/addressing.c
@@ -1,25 +1,39 @@
 
 #include <stdio.h>
 
+void sample(int arr[], int* length);
+
 int main()
 {
 
-	int i, j;
-	int array_size = 20;
-	int arr[array_size][array_size];
+	int i;
+	int array_size = 10;
+	int arr[array_size];
+
+	printf("address of array_size is: %p\n", &array_size);
 
 	for (i = 0; i < array_size; i++) {
 	
-		j = 0;
-		printf("Address of arr[%d][%d] is: %p and contains: %d\n",
-			 i, j, &arr[i][j], arr[i][j]);
-
-		for (j = 0; j < array_size; j++) {
-
-			printf("Address of arr[%d][%d] is: %p and contains: %d\n",
-				 i, j, &arr[i][j], arr[i][j]);
-		}
+		printf("Address of arr[%d] is: %p and contains: %d\n",
+			 i, &arr[i], arr[i]);
 	}
 
+	printf("Entering the test function.\n");
+
+	sample(arr, &array_size);
+
 	return 0;
+}
+
+void sample(int arr[], int* length)
+{
+
+	int i;
+	printf("address of length is: %p\n", &*length);
+
+	for (i = 0; i < *length; i++) {
+	
+		printf("Address of arr[%d] is: %p and contains: %d\n",
+			 i, &arr[i], arr[i]);
+	}
 }
